0937d44a1dc84987c435d8144697d54e2b58a6b4
ananda-0007 <anandank451@gmail.com>
2025-08-20 12:13:43 +0530

    Add files via upload

---
+++ Default,construction.java
@@ -0,0 +1,18 @@
+public class Box {
+    double w, h, d;
+
+    // Constructor
+    Box() {
+        System.out.println("This is constructor");
+        w = 10;
+        h = 10;
+        d = 10;
+        System.out.println("The values of w, h, d are " + w + ", " + h + ", " + d);
+    }
+
+    public static void main(String[] args) {
+        Box Mybox = new Box();
+    }
+} {
+    
+}
